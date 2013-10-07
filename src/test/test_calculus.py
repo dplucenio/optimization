01f1e24789e13b1c4e911c489344a9d4c977e91d
dplucenio <dplucenio@gmail.com>
2013-10-07 20:45:33 -0300

    Adding another hessian test

--- src/test/test_calculus.py
+++ src/test/test_calculus.py
@@ -9,20 +9,20 @@ class Test(unittest.TestCase):
     def testConstantFunction(self):
         def f(x):
             return 1.0
-        assert_almost_equal(gradient(f, [0.0]), [0.0], 6)
+        assert_almost_equal(gradient(f, [0.0]), [0.0], 3)
         
         
     def testLinearFunction(self):
         def f(x):
             return x[0]
-        assert_almost_equal(gradient(f, [0.0]), [1.0], 6)
+        assert_almost_equal(gradient(f, [0.0]), [1.0], 3)
 
 
     def testCubicFunction(self):
         def f(x):
             x1 = x[0]
             return x1**3.0
-        assert_almost_equal(gradient(f, [2.0]), [12.0], 6)
+        assert_almost_equal(gradient(f, [2.0]), [12.0], 3)
         
         
     def testTwoVariableFunction1(self):
@@ -30,21 +30,20 @@ class Test(unittest.TestCase):
             x1 = x[0]
             x2 = x[1]
             return x1**2.0 + x2**2.0
-        assert_almost_equal(gradient(f, [2.0, 2.0]), [4.0, 4.0], 6)
+        assert_almost_equal(gradient(f, [2.0, 2.0]), [4.0, 4.0], 3)
         
         
     def testTwoVariableFunction2(self):
-        
         def f(vector_x):
             x = vector_x[0]
             y = vector_x[1]
             return (x - 2.0)**2.0 + (2.0 * y - 4.0)**2.0
         
-        assert_almost_equal(gradient(f, [2.0, 2.0]), [0.0, 0.0], 6)
-        assert_almost_equal(gradient(f, [0.0, 0.0]), [-4.0, -16.0], 6)
+        assert_almost_equal(gradient(f, [2.0, 2.0]), [0.0, 0.0], 3)
+        assert_almost_equal(gradient(f, [0.0, 0.0]), [-4.0, -16.0], 3)
+
         
     def testHessian1(self):
-        
         def f(x):
             return x[0]**3.0 + x[1]**3.0
         
@@ -54,13 +53,21 @@ class Test(unittest.TestCase):
         
         
     def testHessian2(self):
-        
         def f(x):
             return x[0]**3.0 + x[1]**3.0 + 2.0*x[0]**2.0 + 3.0*x[1]**2.0- x[0]*x[1] + 2.0*x[0] + 4.0*x[1]
         
         x=[1.0, 2.0]
         assert_almost_equal(gradient(f,x), [7.0, 27.0], 3)
         assert_almost_equal(hessian(f,x), [[10.0, -1.0],[-1.0, 18.0]], 2)
+        
+        
+    def testHessian3(self):
+        def f(x):
+            return (2.0*x[1] - 4.0)**2.0 + 2.0
+        
+        x=[2.0, 2.0]
+        assert_almost_equal(gradient(f,x), [0.0, 0.0], 3)
+        assert_almost_equal(hessian(f,x), [[0.0, 0.0],[0.0, 8.0]], 3)
         
         
 if __name__ == "__main__":
